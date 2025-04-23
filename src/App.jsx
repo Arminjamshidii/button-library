e2fb3c91a1e1d1d3f359a3aa2b64e8931b24955f
armin jamshidi <arminjamshidi.dev@gmail.com>
2025-04-23 12:33:07 +0330

    add butto and compelete app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import './App.css'
 
-function App() {
- 
+import './App.css';
+import Button from './components/Button';
 
+function App() {
   return (
     <>
+      {/* Primary button with medium size */}
+      <Button variant="primary" size="md">
+        Primary Button
+      </Button>
+
+      {/* Submit button with small size */}
+      <Button variant="submit" size="sm">
+        Submit Button
+      </Button>
+
+      {/* Secondary button with small size */}
       
+      <Button variant="secondary" size="sm">
+        Secondary Button
+      </Button>
+
+      {/* Delete button with large size */}
+      <Button variant="delete" size="lg">
+        Delete Button
+      </Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,1 +1,21 @@
+import React from 'react';
+import styles from './Button.module.css';
 
+const Button = ({ children, variant = "primary", size = "md" }) => {
+  // Get the variant of class (if not found put primary as default)
+  const variantClass = styles[variant] || styles.primary;
+
+  // Get the size of class (if not found put md as default)
+  const sizeClass = styles[size] || styles.md;
+
+  // Concat all classes
+  const concatClasses = `${styles.button} ${variantClass} ${sizeClass}`;
+
+  return (
+    <button className={concatClasses}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
